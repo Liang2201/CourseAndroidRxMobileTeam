d4ef17c22d42cb0d897481ee4922dcdfd8338fd9
TRAN DUC LUONG <tducluong.individual@gmail.com>
2024-08-23 17:14:19 +0700

    new

--- src/main/java/com/rxmobileteam/lecture2_3/products/product.kt
+++ src/main/java/com/rxmobileteam/lecture2_3/products/product.kt
@@ -1,6 +1,7 @@
 package com.rxmobileteam.lecture2_3.products
 
 import java.util.*
+import java.util.stream.Collectors
 
 enum class ProductCategory {
   LAPTOP,
@@ -25,35 +26,37 @@ data class Order(
 )
 
 // TODO: Return a list of Product, sorted in the ascending by price. if prices are equal, sorted by favoriteCount descending
-fun List<Product>.sortedByPriceAscendingThenByFavoriteCountDescending(): List<Product> = TODO()
-
+fun List<Product>.sortedByPriceAscendingThenByFavoriteCountDescending(): List<Product> =
+  this.sortedWith(compareBy<Product> { it.price }.thenByDescending { it.favoriteCount })
 // TODO: Return a set of Products in the orders (The order doesn't matter).
-fun List<Order>.getProductsSet(): Set<Product> = TODO()
+fun List<Order>.getProductsSet(): Set<Product> = this.filter { it.isDelivered }.flatMap { it.products }.toSet()
 
 // TODO: Return a list of Products in the orders, duplicates are allowed.
-fun List<Order>.getProductsList(): List<Product> = TODO()
+fun List<Order>.getProductsList(): List<Product> = this.flatMap { it.products }
 
 // TODO: Return a list of delivered orders
-fun List<Order>.getDeliveredOrders(): List<Order> = TODO()
+fun List<Order>.getDeliveredOrders(): List<Order> = this.filter { it.isDelivered }
 
 // TODO: Return a list of products in the delivered orders
-fun List<Order>.getDeliveredProductsList(): List<Product> = TODO()
+fun List<Order>.getDeliveredProductsList(): List<Product> = this.filter { it.isDelivered }.flatMap { it.products }
 
 // TODO: Partition the orders into two lists: "delivered" and "not delivered"
-fun List<Order>.partitionDeliveredAndNotDelivered(): Pair<List<Order>, List<Order>> = TODO()
+fun List<Order>.partitionDeliveredAndNotDelivered(): Pair<List<Order>, List<Order>> = this.partition { it.isDelivered }
 
 // TODO: Return a map of product to count of this product in the orders
 // eg. [Product1 -> 2, Product2 -> 1, Product3 -> 3]
-fun List<Order>.countOfEachProduct(): Map<Product, Int> = TODO()
+fun List<Order>.countOfEachProduct(): Map<Product, Int> = this.getProductsList().groupingBy { it }.eachCount()
 
 // TODO: Return the sum of product prices in the order
-fun Order.sumProductPrice(): Double = TODO()
+fun Order.sumProductPrice(): Double = this.products.sumOf(Product::price)
 
 // TODO: Return the product with the maximum price in the order
-fun Order.getMaxPriceProduct(): Product = TODO()
+fun Order.getMaxPriceProduct(): Product =
+  this.products.maxByOrNull { it.price }?:throw NoSuchElementException("No max price available for this order")
 
 // TODO: Return the product with the min price in the order
-fun Order.getMinPriceProduct(): Product = TODO()
+fun Order.getMinPriceProduct(): Product =
+  this.products.minByOrNull { it.price }?:throw NoSuchElementException("No min price available for this order")
 
 val product = Product(
   id = UUID.randomUUID().toString(),
